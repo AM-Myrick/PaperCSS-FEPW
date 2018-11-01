75dd078043a420b06d44214b5a35acd261c2b659
AM_Myrick <amyrick.820@gmail.com>
2018-11-01 11:48:49 -0400

    finishes CSS refactor

---
+++ src/Views/NoteCard.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Route, Link } from "react-router-dom";
+import "./NoteCard.css"
+
+const NoteCard = props => {
+    return (
+        <Link class="card" to={`/note/${props.note._id}`}>
+            {/* <div className="notecard">
+                <h3>{props.note.title}</h3>
+                <p>{props.note.textBody}</p>
+            </div> */}
+            <div class="card-body">
+                <h4 class="card-title">{props.note.title.length > 15 ?
+                    props.note.title.substring(0, 10) + "..." :
+                    props.note.title}</h4>
+                <p class="card-text">{props.note.textBody.length > 75?
+                    props.note.textBody.substring(0, 75) + "..." :
+                    props.note.textBody}</p>
+            </div>
+        </Link>
+    )
+}
+
+export default NoteCard;
