72f5f452cf1c24c9e778b4944be3fdc9518f2852
Andre Myrick <amyrick.820@gmail.com>
2019-04-04 21:54:00 -0400

    updates navigation view and css

--- src/Views/Navigation.js
+++ src/Views/Navigation.js
@@ -16,10 +16,10 @@ export default class Navigation extends Component {
             <nav>
                 <h1>Paper Notes</h1>
                 <div className="btn-container">
-                    <NavLink to="/">Join Paper Notes!</NavLink>
-                    <NavLink to="/login">Login to Paper Notes!</NavLink>
+                    <NavLink to="/">Register</NavLink>
+                    <NavLink to="/login">Login</NavLink>
                     <NavLink to="/all-notes">View Your Notes</NavLink>
-                    <NavLink to="/add-note">+ Create New Note</NavLink>
+                    <NavLink to="/add-note">Add New Note</NavLink>
                     <NavLink to="/login" onClick={() => this.logout()}>Logout</NavLink>
                 </div>
             </nav>
