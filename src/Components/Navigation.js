8f1c0c657bbda587595c74b32267b08b0b359ea3
AM-Myrick <amyrick.820@gmail.com>
2019-10-13 09:45:58 -0400

    updates Navigation for responsivity above 768px

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -7,7 +7,7 @@ export default class Navigation extends Component {
     constructor(props) {
         super(props);
         this.state = {
-          width: 0
+          width: window.innerWidth
         };
     }
 
@@ -37,16 +37,16 @@ export default class Navigation extends Component {
                 </nav> :
                 <nav className="fixed">
                     <h1>Paper Notes</h1>
-                    <div class="collapsible">
+                    <div className="collapsible">
                         <input id="collapsible1" type="checkbox" name="collapsible1" />
                         <button>
-                        <label for="collapsible1">
-                            <div class="bar1"></div>
-                            <div class="bar2"></div>
-                            <div class="bar3"></div>
+                        <label htmlFor="collapsible1">
+                            <div className="bar1"></div>
+                            <div className="bar2"></div>
+                            <div className="bar3"></div>
                         </label>
                         </button>
-                        <div className="collapsible-body btn-container">
+                        <div className={this.state.width <= 768 ? "collapsible-body btn-container" : "btn-container"}>
                             <NavLink to="/">Register</NavLink>
                             <NavLink to="/login">Login</NavLink>
                             <NavLink to="/all-notes">View Your Notes</NavLink>
