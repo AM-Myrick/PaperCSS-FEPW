0fc5bc48cea9294be3e4bd518864f4f94a9daa05
AM-Myrick <amyrick.820@gmail.com>
2019-10-12 22:04:19 -0400

    starts on Nav refactor, still some bugs

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -4,21 +4,57 @@ import "./Navigation.css"
 
 
 export default class Navigation extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          width: 0
+        };
+    }
+
+    componentDidMount() {
+        window.addEventListener('resize', this.updateWidth)
+    }
+
+    updateWidth = () => {
+        this.setState({ width: window.innerWidth })
+    }
+
     logout = () => {
         localStorage.removeItem("access_token");
     }
     render() {
         return (
-            <nav>
-                <h1>Paper Notes</h1>
-                <div className="btn-container">
-                    <NavLink to="/">Register</NavLink>
-                    <NavLink to="/login">Login</NavLink>
-                    <NavLink to="/all-notes">View Your Notes</NavLink>
-                    <NavLink to="/add-note">Add New Note</NavLink>
-                    <NavLink to="/login" onClick={() => this.logout()}>Logout</NavLink>
-                </div>
-            </nav>
+            this.state.width >= 1000 ?
+                <nav>
+                    <h1>Paper Notes</h1>
+                    <div className="btn-container">
+                        <NavLink to="/">Register</NavLink>
+                        <NavLink to="/login">Login</NavLink>
+                        <NavLink to="/all-notes">View Your Notes</NavLink>
+                        <NavLink to="/add-note">Add New Note</NavLink>
+                        <NavLink to="/login" onClick={() => this.logout()}>Logout</NavLink>
+                    </div>
+                </nav> :
+                <nav className="fixed">
+                    <h1>Paper Notes</h1>
+                    <div class="collapsible">
+                        <input id="collapsible1" type="checkbox" name="collapsible1" />
+                        <button>
+                        <label for="collapsible1">
+                            <div class="bar1"></div>
+                            <div class="bar2"></div>
+                            <div class="bar3"></div>
+                        </label>
+                        </button>
+                        <div className="collapsible-body btn-container">
+                            <NavLink to="/">Register</NavLink>
+                            <NavLink to="/login">Login</NavLink>
+                            <NavLink to="/all-notes">View Your Notes</NavLink>
+                            <NavLink to="/add-note">Add New Note</NavLink>
+                            <NavLink to="/login" onClick={() => this.logout()}>Logout</NavLink>
+                        </div>
+                    </div>
+                </nav>
         )
     }
 }
