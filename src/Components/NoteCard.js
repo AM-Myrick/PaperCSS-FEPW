263a4843928565103ac93525783cc4421fd379d7
AM-Myrick <amyrick.820@gmail.com>
2019-10-15 19:30:50 -0400

    adds full responsiveness to all notes page

--- src/Components/NoteCard.js
+++ src/Components/NoteCard.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Link } from "react-router-dom";
 import "./NoteCard.css"
 
+const cardTextSlice = window.innerWidth <= 768 ? 37 : 75
+
 const NoteCard = props => {
     return (
         <Link className="card" to={`/note/${props.note.id}`}>
@@ -9,8 +11,8 @@ const NoteCard = props => {
                 <h4 className="card-title">{props.note.title.length > 33 ?
                     props.note.title.substring(0, 33) + "..." :
                     props.note.title}</h4>
-                <p className="card-text">{props.note.content.length > 75?
-                    props.note.content.substring(0, 75) + "..." :
+                <p className="card-text">{props.note.content.length > cardTextSlice?
+                    props.note.content.substring(0, cardTextSlice) + "..." :
                     props.note.content}</p>
             </div>
         </Link>
