d827ba845fff64aeff77c353e8f79f2047b15a98
AM-Myrick <amyrick.820@gmail.com>
2019-10-14 21:03:46 -0400

    updates responsiveness of AllNotes and NoteCard

--- src/Components/NoteCard.js
+++ src/Components/NoteCard.js
@@ -6,8 +6,8 @@ const NoteCard = props => {
     return (
         <Link className="card" to={`/note/${props.note.id}`}>
             <div className="card-body">
-                <h4 className="card-title">{props.note.title.length > 15 ?
-                    props.note.title.substring(0, 22) + "..." :
+                <h4 className="card-title">{props.note.title.length > 33 ?
+                    props.note.title.substring(0, 33) + "..." :
                     props.note.title}</h4>
                 <p className="card-text">{props.note.content.length > 75?
                     props.note.content.substring(0, 75) + "..." :
