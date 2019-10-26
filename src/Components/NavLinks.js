8b357fc717fd9a5e42adbbd109a4e44ea60843fd
AM-Myrick <amyrick.820@gmail.com>
2019-10-26 16:54:59 -0400

    changes NavLinks to support local storage update

--- src/Components/NavLinks.js
+++ src/Components/NavLinks.js
@@ -3,21 +3,22 @@ import { NavLink } from "react-router-dom";
 
 const NavLinks = props => {
     const token = localStorage.getItem("access_token");
-    const notes = localStorage.getItem("paper_notes");
-    const logout = () => localStorage.removeItem("access_token")
+    const logout = () => localStorage.removeItem("access_token");
 
     return (
-        token === null && notes === null ?    
-        <div className={props.width <= 768 ? "collapsible-body btn-container" : "btn-container"}
-            onClick={() => props.closeMenu()}>
-            <NavLink className="sign-in" to="/">Register</NavLink>
-            <NavLink className="sign-in" to="/login">Login</NavLink>
-        </div> :
+        token ?    
         <div className={props.width <= 768 ? "collapsible-body btn-container" : "btn-container"}
             onClick={() => props.closeMenu()}>
             <NavLink to="/">View Notes</NavLink>
             <NavLink to="/add-note">Add Note</NavLink>
             <NavLink to="/login" onClick={() => logout()}>Logout</NavLink>
+        </div> :
+        <div className={props.width <= 768 ? "collapsible-body btn-container" : "btn-container"}
+            onClick={() => props.closeMenu()}>
+            <NavLink to="/">View Notes</NavLink>
+            <NavLink to="/add-note">Add Note</NavLink>            
+            <NavLink to="/">Register</NavLink>
+            <NavLink to="/login">Login</NavLink>
         </div>
     )
 }
