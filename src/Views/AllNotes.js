9aa0228f6b023f4bac428c9da77b9d013bdd80c9
AM-Myrick <amyrick.820@gmail.com>
2019-10-26 11:39:20 -0400

    makes AllNotes work with local storage

--- src/Views/AllNotes.js
+++ src/Views/AllNotes.js
@@ -1,94 +1,122 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import NoteCard from "../Components/NoteCard";
-import './AllNotes.css';
+import "./AllNotes.css";
 import { CSVLink } from "react-csv";
 import axios from "axios";
 
 const URL = "https://nameless-cliffs-24621.herokuapp.com";
 class AllNotes extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            notes: "",
-            selected: 0,
-            token: ""
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      notes: "",
+      selected: 0
+    };
+  }
 
-    componentDidMount() {
-        axios
-            .get(`${URL}/api/notes`)
-            .then(res => {
-                this.setState({notes: res.data})
-            })
-            .catch(error => {
-              console.log(error)
-            })
-    }
+  componentDidMount() {
+    const token = localStorage.getItem("access_token");
+    const notes = localStorage.getItem("paper_notes");
+    this.getNotes(token, notes);
+  }
 
-    selectNote = id => this.setState({ selected: id });
+  getNotes = (token, notes) => {
+    if (token === null && notes === null) {
+      const instructions = `
+          How to use this site:
 
-    sortNotes = (e, alphaSort) => {
-        e.preventDefault();
-        alphaSort === true ?
-        this.setState(
-            { notes: 
-                this.state.notes.sort(function(a, b){
-                    let titleA=a.title.toLowerCase()
-                    let titleB=b.title.toLowerCase()
-                    if (titleA < titleB) { 
-                        return -1; 
-                    }
-                    if (titleA > titleB) {
-                        return 1;
-                    }
-                    return 0; 
-            })}) :
-            this.setState(
-                { notes: 
-                    this.state.notes.sort(function(b, a){
-                        let titleA=a.title.toLowerCase()
-                        let titleB=b.title.toLowerCase()
-                        if (titleA < titleB) { 
-                            return -1; 
-                        }
-                        if (titleA > titleB) {
-                            return 1;
-                        }
-                        return 0; 
-                })})
+          If you create notes without being logged in, they'll be saved to local storage.
+          If you create an account while you have notes in local storage, they'll be saved to your new account and removed from local storage.
+
+          Thanks for checking out my project! If you're interested in seeing more or contacting me, go to <a href="https://youwontregrethiring.me">my portfolio</a>.
+          `;
+      const notes = [
+        { id: "note-1", title: "Welcome to Paper Notes", content: instructions }
+      ];
+
+      localStorage.setItem("paper_notes", JSON.stringify(notes));
+      this.setState({ notes });
+    } else if (token) {
+      axios
+        .get(`${URL}/api/notes`)
+        .then(res => {
+          this.setState({ notes: res.data });
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    } else {
+      this.setState({ notes: JSON.parse(notes) });
     }
-     
-    headers = [
-        { label: "Title", key: "title" },
-        { label: "Note Content", key: "content" },
-    ];
+  };
+
+  selectNote = id => this.setState({ selected: id });
+
+  sortNotes = (e, alphaSort) => {
+    e.preventDefault();
+    alphaSort === true
+      ? this.setState({
+          notes: this.state.notes.sort(function(a, b) {
+            let titleA = a.title.toLowerCase();
+            let titleB = b.title.toLowerCase();
+            if (titleA < titleB) {
+              return -1;
+            }
+            if (titleA > titleB) {
+              return 1;
+            }
+            return 0;
+          })
+        })
+      : this.setState({
+          notes: this.state.notes.sort(function(b, a) {
+            let titleA = a.title.toLowerCase();
+            let titleB = b.title.toLowerCase();
+            if (titleA < titleB) {
+              return -1;
+            }
+            if (titleA > titleB) {
+              return 1;
+            }
+            return 0;
+          })
+        });
+  };
+
+  headers = [
+    { label: "Title", key: "title" },
+    { label: "Note Content", key: "content" }
+  ];
 
   render() {
-    return (
-        this.state.notes ?
-        <section onClick={() => this.props.closeMenu()}>
-            <div className="btn-container">
-                <CSVLink download="mynotes.csv" data={this.state.notes} headers={this.headers}>
-                    Download your notes!
-                </CSVLink>
-                <div className="dropdown">
-                    <button className="dropbtn">Sort Options</button>
-                    <div className="dropdown-content">
-                        <div onClick={(e) => this.sortNotes(e, true)}>A-Z</div>
-                        <div onClick={(e) => this.sortNotes(e, false)}>Z-A</div>
-                    </div>
-                </div>
+    return this.state.notes ? (
+      <section onClick={() => this.props.closeMenu()}>
+        <div className="btn-container">
+          <CSVLink
+            download="mynotes.csv"
+            data={this.state.notes}
+            headers={this.headers}
+          >
+            Download your notes!
+          </CSVLink>
+          <div className="dropdown">
+            <button className="dropbtn">Sort Options</button>
+            <div className="dropdown-content">
+              <div onClick={e => this.sortNotes(e, true)}>A-Z</div>
+              <div onClick={e => this.sortNotes(e, false)}>Z-A</div>
             </div>
-            <h2>Your Notes:</h2>
-            <div className="notes">
-                {this.state.notes.map(note => {
-                    return <NoteCard key={note.id} note={note} />
-                })}
-            </div>
-        </section> :
-        <h3>Your Notes are loading...</h3>
-    )
+          </div>
+        </div>
+        <h2>Your Notes:</h2>
+        <div className="notes">
+          {this.state.notes.map(note => {
+            return <NoteCard key={note.id} note={note} />;
+          })}
+        </div>
+      </section>
+    ) : (
+      <h3>Your Notes are loading...</h3>
+    );
   }
 }
 
