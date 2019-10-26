cc5d3c54565be332e2530da34732f7a06672fdfa
AM-Myrick <amyrick.820@gmail.com>
2019-10-26 17:26:01 -0400

    makes navlinks stateful

--- src/Components/NavLinks.js
+++ src/Components/NavLinks.js
@@ -1,26 +1,49 @@
-import React from 'react';
+import React from "react";
 import { NavLink } from "react-router-dom";
 
-const NavLinks = props => {
-    const token = localStorage.getItem("access_token");
-    const logout = () => localStorage.removeItem("access_token");
+const token = localStorage.getItem("access_token");
+const logout = () => localStorage.removeItem("access_token");
+export default class NavLinks extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      register: false,
+      signin: false
+    };
+  }
 
-    return (
-        token ?    
-        <div className={props.width <= 768 ? "collapsible-body btn-container" : "btn-container"}
-            onClick={() => props.closeMenu()}>
-            <NavLink to="/">View Notes</NavLink>
-            <NavLink to="/add-note">Add Note</NavLink>
-            <NavLink to="/login" onClick={() => logout()}>Logout</NavLink>
-        </div> :
-        <div className={props.width <= 768 ? "collapsible-body btn-container" : "btn-container"}
-            onClick={() => props.closeMenu()}>
-            <NavLink to="/">View Notes</NavLink>
-            <NavLink to="/add-note">Add Note</NavLink>            
-            <NavLink to="/">Register</NavLink>
-            <NavLink to="/login">Login</NavLink>
-        </div>
-    )
+  
+
+  render() {
+    return token ? (
+      <div
+        className={
+          this.props.width <= 768
+            ? "collapsible-body btn-container"
+            : "btn-container"
+        }
+        onClick={() => this.props.closeMenu()}
+      >
+        <NavLink to="/">View Notes</NavLink>
+        <NavLink to="/add-note">Add Note</NavLink>
+        <NavLink to="/login" onClick={() => logout()}>
+          Logout
+        </NavLink>
+      </div>
+    ) : (
+      <div
+        className={
+          this.props.width <= 768
+            ? "collapsible-body btn-container"
+            : "btn-container"
+        }
+        onClick={() => this.props.closeMenu()}
+      >
+        <NavLink to="/">View Notes</NavLink>
+        <NavLink to="/add-note">Add Note</NavLink>
+        <NavLink to="/">Register</NavLink>
+        <NavLink to="/login">Login</NavLink>
+      </div>
+    );
+  }
 }
-
-export default NavLinks;
